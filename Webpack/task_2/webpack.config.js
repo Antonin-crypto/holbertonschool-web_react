10c536c2cdc17f60d8221c3f4ab3bd4c4c18517d
Antonin-crypto <akamis53@gmail.com>
2025-04-16 09:40:06 +0200

    update

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,55 +1,35 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
+  mode: "production",
+  entry: "./js/dashboard_main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    clean: true,
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[hash][ext][query]',
-        },
         use: [
           {
-            loader: 'image-webpack-loader',
+            loader: "file-loader",
+          },
+          {
+            loader: "image-webpack-loader",
             options: {
               mozjpeg: {
                 progressive: true,
                 quality: 65,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
               },
             },
           },
         ],
       },
     ],
-  },
-  performance: {
-    hints: false,
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
   },
 };
