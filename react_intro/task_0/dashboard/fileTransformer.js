a721275dd64e028deeef016cf9eab44b218d4cfe
Antonin-crypto <akamis53@gmail.com>
2025-04-22 10:20:32 +0200

    modifie package

--- react_intro/task_0/dashboard/fileTransformer.js
+++ react_intro/task_0/dashboard/fileTransformer.js
@@ -1,5 +1,9 @@
-module.exports = {
-  process() {
-    return "module.exports = {};";
+import path from "path";
+
+export default {
+  process(sourceText, sourcePath, options) {
+    return {
+      code: `module.exports = ${JSON.stringify(path.basename(sourcePath))};`,
+    };
   },
 };
