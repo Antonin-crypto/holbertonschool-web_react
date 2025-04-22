61333ea8b652ccc00bbfb33495a8bab94d4ffc1b
Antonin-crypto <akamis53@gmail.com>
2025-04-22 13:02:17 +0200

    modify app

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -15,6 +15,12 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">password:</label>
+        <input type="password" id="password" name="password" />
+        <button type="submit">OK</button>
       </div>
       <div className="App-footer">
         <p>
