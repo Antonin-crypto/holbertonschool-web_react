f8137827f05e723d14f8cd9b6590ee4c713de277
Antonin-crypto <akamis53@gmail.com>
2025-04-22 11:53:56 +0200

    modifie requirement

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-describe("App component", () => {
-  test('renders h1 with text "School dashboard"', () => {
+describe('App component', () => {
+  test('renders h1 with text 'School dashboard'', () => {
     render(<App />);
-    const heading = screen.getByRole("heading", { name: /school dashboard/i });
+    const heading = screen.getByRole('heading', { name: /school dashboard/i });
     expect(heading).toBeInTheDocument();
   });
 
-  test("renders correct text in body and footer", () => {
+  test('renders correct text in body and footer', () => {
     render(<App />);
 
     // Paragraph in App-body
@@ -19,12 +19,12 @@ describe("App component", () => {
     // Paragraph in App-footer
     const currentYear = new Date().getFullYear();
     const footerText = screen.getByText(
-      new RegExp(`©? ?${currentYear} - holberton school`, "i")
+      new RegExp(`©? ?${currentYear} - holberton school`, 'i')
     );
     expect(footerText).toBeInTheDocument();
   });
 
-  test("renders the logo image", () => {
+  test('renders the logo image', () => {
     render(<App />);
     const image = screen.getByAltText(/holberton logo/i);
     expect(image).toBeInTheDocument();
