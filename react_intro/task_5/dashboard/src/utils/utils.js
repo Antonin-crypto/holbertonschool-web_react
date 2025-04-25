eccf1e04786e79291221d8e4f2a1e72e56f5d86a
Antonin-crypto <akamis53@gmail.com>
2025-04-25 10:52:44 +0200

    test

--- react_intro/task_5/dashboard/src/utils/utils.js
+++ react_intro/task_5/dashboard/src/utils/utils.js
@@ -1,9 +1,13 @@
 export function getCurrentYear() {
   return new Date().getFullYear();
 }
-
+// داخل utils.js أو الملف الذي يحتوي الدالة
 export function getFooterCopy(isIndex) {
-  return isIndex ? "holberton School" : "Holberton School main dashboard";
+  if (isIndex) {
+    return "Holberton School";
+  } else {
+    return "Holberton School main dashboard"; // يجب أن تُرجع هذه القيمة عند `false`
+  }
 }
 
 export function getLatestNotification() {
