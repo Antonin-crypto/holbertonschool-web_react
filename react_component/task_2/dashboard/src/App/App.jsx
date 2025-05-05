e92dc48cf7c451ec30d179471415bfefe7c09e3a
Antonin-crypto <akamis53@gmail.com>
2025-05-05 15:55:31 +0200

    Handling Events

---
+++ react_component/task_2/dashboard/src/App/App.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import "./App.css";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import PropTypes from "prop-types";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+    };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  render() {
+    const notificationsList = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      {
+        id: 3,
+        type: "urgent",
+        html: {
+          __html: "<strong>Urgent requirement</strong> - complete by EOD",
+        },
+      },
+    ];
+
+    const coursesList = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
+    return (
+      <React.Fragment>
+        <div className="root-notifications">
+          <Notifications notifications={notificationsList} />
+        </div>
+        <Header />
+        {this.state.isLoggedIn ? (
+          <CourseList courses={coursesList} />
+        ) : (
+          <Login />
+        )}
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+App.defaultProps = {
+  logOut: () => {},
+};
+
+App.propTypes = {
+  logOut: PropTypes.func,
+};
+
+export default App;
