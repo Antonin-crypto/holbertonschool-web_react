42180aabc644e302da2cad700e3a99d79c7ea30f
Antonin-crypto <akamis53@gmail.com>
2025-06-06 10:36:50 +0200

    update

--- react_redux-part1/task_1/dashboard/src/app/rootReducer.js
+++ react_redux-part1/task_1/dashboard/src/app/rootReducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-import authReducer from "./auth/authSlice";
-import notificationsReducer from "./notifications/notificationsSlice";
-import coursesReducer from "./courses/coursesSlice";
+import authReducer from "../features/auth/authSlice";
+import notificationsReducer from "../features/notifications/notificationsSlice";
+import coursesReducer from "../features/courses/coursesSlice";
 
 const rootReducer = combineReducers({
   auth: authReducer,
