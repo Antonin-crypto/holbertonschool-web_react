a60d783be63cf54feb0049ae03a259f220689e5f
Antonin-crypto <akamis53@gmail.com>
2025-06-09 10:01:29 +0200

    Synchronize Footer with Redux store

---
+++ react_redux-part1/task_4/dashboard/src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+import { useSelector } from "react-redux";
+import { getCurrentYear, getFooterCopy } from "../../utils/utils";
+
+export default function Footer() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  return (
+    <div className={css(styles.footer)}>
+      <p>
+        Copyright {getCurrentYear()} - {getFooterCopy(true)}
+      </p>
+      {isLoggedIn && <a href="#">Contact us</a>}
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  footer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontStyle: "italic",
+    fontFamily: "sans-serif",
+  },
+});
