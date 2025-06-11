e29235766d6c1910f0b54ae02a012446a0e3d4ca
Antonin-crypto <akamis53@gmail.com>
2025-06-11 09:50:55 +0200

    update

--- react_redux-part1/task_3/dashboard/src/components/Header/Header.jsx
+++ react_redux-part1/task_3/dashboard/src/components/Header/Header.jsx
@@ -30,29 +30,32 @@ const styles = StyleSheet.create({
 export default function Header() {
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  console.log("isLoggedIn in Header:", isLoggedIn);
   const user = useSelector((state) => state.auth.user);
-
+  console.log("App.jsx - isLoggedIn:", isLoggedIn);
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(logout());
   };
-
+  console.log("isLoggedIn:", isLoggedIn);
+  console.log("logout:", logout);
+  console.log("handleLogout:", handleLogout);
   return (
     <div className={css(styles.header)}>
       <img src={logo} className={css(styles.logo)} alt="holberton logo" />
       <h1 className={css(styles.title)}>School Dashboard</h1>
-      <div className={css(styles.logoutSection)} id="logoutSection">
-        {isLoggedIn ? (
-          <>
-            Welcome <b>{user?.email}</b>{" "}
-            <a href="#" onClick={handleLogout}>
-              (logout)
-            </a>
-          </>
-        ) : (
-          <p>Not logged in</p>
-        )}
-      </div>
+      {isLoggedIn && (
+        <div
+          className={css(styles.logoutSection)}
+          id="logoutSection"
+          data-testid="logoutSection"
+        >
+          Welcome <b>{user?.email}</b>{" "}
+          <a href="#" onClick={handleLogout}>
+            (logout)
+          </a>
+        </div>
+      )}
     </div>
   );
 }
--- react_redux-part1/task_3/dashboard/src/features/auth/authSlice.js
+++ react_redux-part1/task_3/dashboard/src/features/auth/authSlice.js
@@ -1,26 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   user: {
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   },
   isLoggedIn: false,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {
     login: (state, action) => {
       const { email, password } = action.payload;
+      console.log("Dispatching login action with:", { email, password });
+      console.log("Login action payload:", action.payload);
+      console.log("action login true", email);
       state.user.email = email;
       state.user.password = password;
       state.isLoggedIn = true;
     },
     logout: (state) => {
-      state.user.email = '';
-      state.user.password = '';
+      state.user.email = "";
+      state.user.password = "";
+      console.log("action login true", "");
       state.isLoggedIn = false;
     },
   },
