1649b07d03b928d83c86dedd8a836393ba31187c
Antonin-crypto <akamis53@gmail.com>
2025-06-13 09:44:27 +0200

    good projet

---
+++ react_redux-part2/task_0/dashboard/src/components/BodySectionWithMarginBottom/BodySectionWithMarginBottom.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, css } from "aphrodite";
+import BodySection from "../BodySection/BodySection";
+
+export default function BodySectionWithMarginBottom(props) {
+  return (
+    <div
+      className={css(styles.bodySectionWithMargin)}
+      data-testid="body-section-with-margin"
+    >
+      <BodySection {...props} />
+    </div>
+  );
+}
+
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40px",
+  },
+});
